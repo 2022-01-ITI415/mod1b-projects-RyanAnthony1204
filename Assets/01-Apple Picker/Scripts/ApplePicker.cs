a5056fbbccbc2951c9366ba2f0a93e5f2155efae
RyanAnthony1204 <racevedo1204@gmail.com>
2022-02-03 13:30:12 -0500

    Updated ApplePicker
    When an apple is destroyed by reaching the lowest height possible (aka missing it with the basket) it will now delete one of the three baskets until there are none left.

--- Assets/01-Apple Picker/Scripts/ApplePicker.cs
+++ Assets/01-Apple Picker/Scripts/ApplePicker.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ApplePicker : MonoBehaviour
 {
@@ -10,15 +11,19 @@ public class ApplePicker : MonoBehaviour
     public int numBaskets = 3;
     public float basketBottomY = -14f;
     public float basketSpacingY = 2f;
+    public List<GameObject> basketList;
 
     // Start is called before the first frame update
     void Start()
     {
+        basketList = new List<GameObject>();
+
         for (int i = 0; i < numBaskets; i ++){
             GameObject tBasketGO = Instantiate<GameObject>(basketPrefab);
             Vector3 pos = Vector3.zero;
             pos.y  = basketBottomY + (basketSpacingY * i);
             tBasketGO.transform.position = pos;
+            basketList.Add(tBasketGO);
         }
     }
 
@@ -30,5 +35,16 @@ public class ApplePicker : MonoBehaviour
         {
             Destroy(tGO);
         }
+
+        //Destroy one of the baskets
+        //Get the index of the last basket in the basketList
+        int basketIndex = basketList.Count - 1;
+        
+        //Get a reference to the Basket GameObject
+        GameObject tBasketGO = basketList[basketIndex];
+
+        //Remove the basket from the list and destroy the GameObject
+        basketList.RemoveAt(basketIndex);
+        Destroy(tBasketGO);
     }
 }
