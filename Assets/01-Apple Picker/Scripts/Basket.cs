1a1a94a9726f9430b1ecda1fa07a4cddf0fd7763
RyanAnthony1204 <racevedo1204@gmail.com>
2022-02-03 13:12:13 -0500

    Added GUI Text
    created HighScore and ScoreCounter and adjusted Basket script to display correct scoring values based on collisions between the basket and apples.

--- Assets/01-Apple Picker/Scripts/Basket.cs
+++ Assets/01-Apple Picker/Scripts/Basket.cs
@@ -1,10 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Basket : MonoBehaviour
 {
-    // Update is called once per frame
+    [Header("Set Dynamically")]
+
+    public Text scoreGT;
+
+    void Start()
+    {
+        //Find a reference to the ScoreCounter GameObject
+        GameObject scoreGO = GameObject.Find("ScoreCounter");
+
+        //Get the text component of that GameObject
+        scoreGT = scoreGO.GetComponent<Text>();
+
+        //Set the starting number of points to 0
+
+        scoreGT.text = "0";
+    }
+
     void Update()
     {
         //Get the current screen position of the mouse from input
@@ -30,5 +47,14 @@ public class Basket : MonoBehaviour
         {
             Destroy(collidedWith);
         }
+
+        //Parse the text of scoreGT into an int
+        int score = int.Parse(scoreGT.text);
+
+        //Add points for catching the apple
+        score += 100;
+
+        //Convert the score back into a string and display it
+        scoreGT.text = score.ToString();
     }
 }
