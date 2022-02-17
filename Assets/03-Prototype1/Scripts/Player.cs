56597c6994b2565bab52bb58aa1405995e991be8
RyanAnthony1204 <racevedo1204@gmail.com>
2022-02-17 14:45:39 -0500

    Player Movement
    Created the code necessary to facilitate player movement

--- Assets/03-Prototype1/Scripts/Player.cs
+++ Assets/03-Prototype1/Scripts/Player.cs
@@ -1,18 +1,62 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using TMPro;
 
-public class Player : MonoBehaviour
+public class Player: MonoBehaviour
 {
-    // Start is called before the first frame update
+    [Header("Set in Inspector")]
+    public float playerSpeed;
+    public TextMeshProUGUI pointsDisplay;
+    public GameObject winDisplay;
+    private Rigidbody rb;
+    private int points;
+    private float movementX;
+    private float movementY;
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
+        points = 0;
+
+        ScoreCounter();
+        winDisplay.SetActive(false);
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnMove(InputValue movementValue)
     {
-        
+        Vector2 movementVector = movementValue.Get<Vector2>();
+
+        movementX = movementVector.x;
+        movementY = movementVector.y;
     }
+
+    void ScoreCounter()
+    {
+        pointsDisplay.text = "Score: " + points.ToString();
+        if(points >= 12)
+        {
+            winDisplay.SetActive(true);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 movement = new Vector3(movementX, 0.0f, movementY);
+
+        rb.AddForce(movement * playerSpeed);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Point"))
+        {
+            other.gameObject.SetActive(false);
+            points = points + 1;
+
+            ScoreCounter();
+        }
+    }
+
 }
