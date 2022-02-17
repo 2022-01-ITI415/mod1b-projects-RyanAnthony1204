cb63acb3875d360e7891fb7e927ec19c3cbbb6fd
RyanAnthony1204 <racevedo1204@gmail.com>
2022-02-17 13:43:05 -0500

    Added Rotator
    Added the rotator script to give the collectible points some character

--- Assets/03-Prototype1/Scripts/Rotator.cs
+++ Assets/03-Prototype1/Scripts/Rotator.cs
@@ -4,15 +4,8 @@ using UnityEngine;
 
 public class Rotator : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void Update()
     {
-        
+        transform.Rotate(new Vector3(280,140,360) * Time.deltaTime);
     }
 }
